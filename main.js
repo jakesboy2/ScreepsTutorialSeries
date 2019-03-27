40b122ba67da3cdfd6ed24779042f0cba6d594f3
jakesboy2 <jcwaldrip@att.net>
2019-03-27 02:15:35 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+// Main file for the Screeps Tutorial Series by Jacob Waldrip
+// Started March 2019, last updated ${March 2019}
+// The game runs the loop in this file, so all other files must be imported and used here
