f1218fccc662489c79c55624b076cf347bb6314e
jakesboy2 <jcwaldrip@att.net>
2019-05-24 15:22:57 -0500

    uploaded tutorial 1 code

--- Tutorial_1/main.js
+++ Tutorial_1/main.js
@@ -1,1 +1,4 @@
 // Main for tutorial 1
+module.exports.loop = function () {
+    console.log("Heather");    
+}
--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 // Main file for the Screeps Tutorial Series by Jacob Waldrip
-// Started March 2019, last updated ${March 2019}
+// Started May 2019, last updated ${May 2019}
 // The game runs the loop in this file, so all other files must be imported and used here
+module.exports.loop = function () {
+    
+}
