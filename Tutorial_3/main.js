3a64775f6393cb58af9f2889192b740351035c58
jakesboy2 <jcwaldrip@att.net>
2019-06-16 19:59:15 -0500

    moved into its own file for tut 2 and 3

--- Tutorial_3/main.js
+++ Tutorial_3/main.js
@@ -1,1 +1,17 @@
-// Main for tutorial 3
+// Main for tutorial 2
+// Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], Game.time, {memory: {working: false, role: 'harvester'}});
+// Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], Game.time, {memory: {working: false, role: 'worker'}});
+
+const roles = {
+    harvester: require('./Tutorial_2/HarvesterManager'),
+    worker: require('./Tutorial_2/WorkerManager'),
+}
+
+module.exports.loop = function () {
+
+    // Run the harvester manager for every creep in the game
+    for(const key in Game.creeps) {
+        const creep = Game.creeps[key];
+        roles[creep.memory.role].runRole(creep);
+    }
+}
